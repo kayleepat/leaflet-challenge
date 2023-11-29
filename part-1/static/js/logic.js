9e115991ca6517710df3a9f0a68a1ff5415c4f8a
Kaylee Paterson <kayleepat@gmail.com>
2023-11-29 00:41:11 -0500

    added map and markers

---
+++ part-1/static/js/logic.js
@@ -0,0 +1,55 @@
+// draw map
+var map = L.map('map').setView([28.60259692686176, -81.19946981796008], 2);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+d3.json(link).then(function(data) {
+
+    
+    L.geoJson(data, {
+
+        pointToLayer: function(feature) {
+            // Customize the circle marker style based on magnitude
+            return L.circleMarker(feature.geometry.coordinates, {
+                radius: feature.properties.mag * 1.5,
+                fillColor: getColor(feature.geometry.coordinates), 
+                color: '#000',
+                weight: 1,
+                opacity: 0.6,
+                fillOpacity: 0.8
+            });
+        },
+
+        onEachFeature: function(feature, layer) {
+            
+            layer.bindPopup(`
+            <h1>${feature.properties.place}</h1>
+            <hr>
+            <h2>Magnitude: ${feature.properties.mag}</h2>
+            <h2>Depth: ${feature.geometry.coordinates[2]}</h2>
+            <strong>Time:</strong> ${new Date(feature.properties.time)}
+            `);
+        
+        }
+
+    }).addTo(map);
+
+
+})
+
+
+function getColor(coords) {
+    let depth = coords[2];
+
+    return depth >= 90 ? '#FE1F23' :
+           depth >= 70 ? '#FF8E28' :
+           depth >= 50 ? '#FEC627' :
+           depth >= 30 ? '#F0FF39' :
+           depth >= 10 ? '#85F928' :
+           '#28E700';
+}
